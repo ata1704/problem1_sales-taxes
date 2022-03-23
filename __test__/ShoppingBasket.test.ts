4c7ed6325aee0d642717f5ebb53f7ebbb1210164
marcelschalk <ata1704@gmail.com>
2022-03-23 12:57:08 +0100

    Added more testing for checkOut

--- __test__/ShoppingBasket.test.ts
+++ __test__/ShoppingBasket.test.ts
@@ -1,19 +1,100 @@
 import { ShoppingBasket } from '../src/ShoppingBasket';
 import { Product, ProductCategory } from '../src/Product';
 
-describe('ShoppingBasket testing', () => {
-  it('should return the correct total', () => {
-    const shoppingBasket = new ShoppingBasket();
+describe('ShoppingBasket checker', () => {
+  let shoppingBasket: ShoppingBasket;
+  beforeEach(() => {
+    shoppingBasket = new ShoppingBasket();
+  });
+
+  it('exists', () => {
+    expect(shoppingBasket).toBeDefined();
+  });
+
+  it('must not apply basic tax to books', () => {
+    const price = 12.49;
+    shoppingBasket.addShoppingItem(
+      new Product('book', 1, price, ProductCategory.Book, false)
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(price);
+  });
+
+  it('must not apply basic tax to food', () => {
+    const price = 0.85;
+    shoppingBasket.addShoppingItem(
+      new Product('chocolate bar', 1, price, ProductCategory.Food, false)
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(price);
+  });
+
+  it('must not apply basic tax to medical products', () => {
+    const price = 9.75;
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'packet of headache pills',
+        1,
+        price,
+        ProductCategory.MedicalProduct,
+        false
+      )
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(price);
+  });
+
+  it('should apply basic tax to other products than books, food or medical products', () => {
+    shoppingBasket.addShoppingItem(
+      new Product('music CD', 1, 14.99, ProductCategory.Other, false)
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(16.49);
+  });
+
+  it('should apply import duty on imported products', () => {
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported box of chocolates',
+        1,
+        10.0,
+        ProductCategory.Food,
+        true
+      )
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(10.5);
+  });
+
+  it('should total the price of multiple items with various quantities including taxes', () => {
     shoppingBasket.addShoppingItem(
       new Product('book', 1, 12.49, ProductCategory.Book, false)
     );
     shoppingBasket.addShoppingItem(
-      new Product('music CD', 1, 14.99, ProductCategory.Other, false)
+      new Product('chocolate bar', 5, 0.85, ProductCategory.Food, true)
     );
     shoppingBasket.addShoppingItem(
-      new Product('book', 1, 0.85, ProductCategory.Food, false)
+      new Product(
+        'packet of headache pills',
+        1,
+        9.75,
+        ProductCategory.MedicalProduct,
+        false
+      )
     );
-
-    expect(shoppingBasket.checkOut().total).toBe(29.83);
+    shoppingBasket.addShoppingItem(
+      new Product('music CD ', 2, 14.99, ProductCategory.Other, false)
+    );
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported bottle of perfume',
+        1,
+        27.99,
+        ProductCategory.Other,
+        true
+      )
+    );
+    const receipt = shoppingBasket.checkOut();
+    expect(receipt.total).toBe(91.91);
   });
 });
