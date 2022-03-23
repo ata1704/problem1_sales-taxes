0f35c55b0993d7054bbea13c7eb5d4fe00c010f5
marcelschalk <ata1704@gmail.com>
2022-03-23 13:11:31 +0100

    Tests for getReceipt and example data in index.ts

--- __test__/ShoppingBasket.test.ts
+++ __test__/ShoppingBasket.test.ts
@@ -1,7 +1,7 @@
 import { ShoppingBasket } from '../src/ShoppingBasket';
 import { Product, ProductCategory } from '../src/Product';
 
-describe('ShoppingBasket checker', () => {
+describe('checkOut testing', () => {
   let shoppingBasket: ShoppingBasket;
   beforeEach(() => {
     shoppingBasket = new ShoppingBasket();
@@ -96,5 +96,99 @@ describe('ShoppingBasket checker', () => {
     );
     const receipt = shoppingBasket.checkOut();
     expect(receipt.total).toBe(91.91);
+  });
+});
+
+describe('Receipt output testing', () => {
+  let shoppingBasket: ShoppingBasket;
+  beforeEach(() => {
+    shoppingBasket = new ShoppingBasket();
+  });
+
+  it('should correctly output given input example 1', () => {
+    shoppingBasket.addShoppingItem(
+      new Product('book', 1, 12.49, ProductCategory.Book, false)
+    );
+    shoppingBasket.addShoppingItem(
+      new Product('music CD', 1, 14.99, ProductCategory.Other, false)
+    );
+    shoppingBasket.addShoppingItem(
+      new Product('chocolate bar', 1, 0.85, ProductCategory.Food, false)
+    );
+    expect(shoppingBasket.getReceipt()).toBe(
+      '1 book: 12.49\n' +
+        '1 music CD: 16.49\n' +
+        '1 chocolate bar: 0.85\n' +
+        'Sales Taxes: 1.50\n' +
+        'Total: 29.83'
+    );
+  });
+
+  it('should correctly output given input example 2', () => {
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported box of chocolates',
+        1,
+        10.0,
+        ProductCategory.Food,
+        true
+      )
+    );
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported bottle of perfume',
+        1,
+        47.5,
+        ProductCategory.Other,
+        true
+      )
+    );
+    expect(shoppingBasket.getReceipt()).toBe(
+      '1 imported box of chocolates: 10.50\n' +
+        '1 imported bottle of perfume: 54.65\n' +
+        'Sales Taxes: 7.65\n' +
+        'Total: 65.15'
+    );
+  });
+
+  it('should correctly output given input example 3', () => {
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported bottle of perfume',
+        1,
+        27.99,
+        ProductCategory.Other,
+        true
+      )
+    );
+    shoppingBasket.addShoppingItem(
+      new Product('bottle of perfume', 1, 18.99, ProductCategory.Other, false)
+    );
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'packet of headache pills',
+        1,
+        9.75,
+        ProductCategory.MedicalProduct,
+        false
+      )
+    );
+    shoppingBasket.addShoppingItem(
+      new Product(
+        'imported box of chocolates',
+        1,
+        11.25,
+        ProductCategory.Food,
+        true
+      )
+    );
+    expect(shoppingBasket.getReceipt()).toBe(
+      '1 imported bottle of perfume: 32.19\n' +
+        '1 bottle of perfume: 20.89\n' +
+        '1 packet of headache pills: 9.75\n' +
+        '1 imported box of chocolates: 11.85\n' +
+        'Sales Taxes: 6.70\n' +
+        'Total: 74.68'
+    );
   });
 });
--- index.ts
+++ index.ts
@@ -1,1 +1,74 @@
-console.log('test');
+import { Product, ProductCategory } from './src/Product';
+import { ShoppingBasket } from './src/ShoppingBasket';
+
+// Input 1
+const shoppingBasket1 = new ShoppingBasket();
+shoppingBasket1.addShoppingItem(
+  new Product('book', 1, 12.49, ProductCategory.Book, false)
+);
+shoppingBasket1.addShoppingItem(
+  new Product('music CD', 1, 14.99, ProductCategory.Other, false)
+);
+shoppingBasket1.addShoppingItem(
+  new Product('chocolate bar', 1, 0.85, ProductCategory.Food, false)
+);
+
+// Input 2
+const shoppingBasket2 = new ShoppingBasket();
+shoppingBasket2.addShoppingItem(
+  new Product('imported box of chocolates', 1, 10.0, ProductCategory.Food, true)
+);
+shoppingBasket2.addShoppingItem(
+  new Product(
+    'imported bottle of perfume',
+    1,
+    47.5,
+    ProductCategory.Other,
+    true
+  )
+);
+
+// Input 3
+const shoppingBasket3 = new ShoppingBasket();
+shoppingBasket3.addShoppingItem(
+  new Product(
+    'imported bottle of perfume',
+    1,
+    27.99,
+    ProductCategory.Other,
+    true
+  )
+);
+shoppingBasket3.addShoppingItem(
+  new Product('bottle of perfume', 1, 18.99, ProductCategory.Other, false)
+);
+shoppingBasket3.addShoppingItem(
+  new Product(
+    'packet of headache pills',
+    1,
+    9.75,
+    ProductCategory.MedicalProduct,
+    false
+  )
+);
+shoppingBasket3.addShoppingItem(
+  new Product(
+    'imported box of chocolates',
+    1,
+    11.25,
+    ProductCategory.Food,
+    true
+  )
+);
+
+// Output 1
+console.log('Output 1:');
+console.log(shoppingBasket1.getReceipt());
+
+// Output 2
+console.log('\nOutput 2:');
+console.log(shoppingBasket2.getReceipt());
+
+// Output 1
+console.log('\nOutput 3:');
+console.log(shoppingBasket3.getReceipt());
