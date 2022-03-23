db231de3cce548d83ffcb363999f7580696c9dd3
marcelschalk <ata1704@gmail.com>
2022-03-23 12:00:31 +0100

    Logic for the checkOut method in ShoppingBasket

--- src/ShoppingBasket.ts
+++ src/ShoppingBasket.ts
@@ -1,4 +1,5 @@
-import { Product } from './Product';
+import { Product, ProductCategory } from './Product';
+import { ReceiptItem } from './ReceiptItem';
 
 export class ShoppingBasket {
   readonly basicTax = 10;
@@ -8,5 +9,35 @@ export class ShoppingBasket {
 
   addShoppingItem(shoppingItem: Product) {
     this.shoppingBasket.push(shoppingItem);
+  }
+
+  checkOut() {
+    const receiptItems: ReceiptItem[] = [];
+    let salesTaxes: number;
+    let total: number;
+
+    this.shoppingBasket.forEach((shoppingBasketItem) => {
+      let taxRate = 0;
+
+      if (
+        [
+          ProductCategory.Book,
+          ProductCategory.Food,
+          ProductCategory.MedicalProduct,
+        ].includes(shoppingBasketItem.category)
+      )
+        taxRate += this.basicTax;
+
+      if (shoppingBasketItem.isImported) taxRate += this.importDuty;
+
+      receiptItems.push(
+        new ReceiptItem(
+          shoppingBasketItem.name,
+          shoppingBasketItem.quantity,
+          shoppingBasketItem.price,
+          taxRate
+        )
+      );
+    });
   }
 }
